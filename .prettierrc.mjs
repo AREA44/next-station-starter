5bef6262b158c267224ad8e9434a8e2b4fdb71a6
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-11 15:05:59 +0700

    Setup the site config (#3)

---
+++ .prettierrc.mjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
-import './global.css'
-import type { Metadata } from 'next'
+import './global.css';
+import type { Metadata } from 'next';
+
+const url = 'https://next-valhalla-starter.netlify.app';
+const title = 'Next Valhalla Starter | AREA44';
+const description = 'An image gallery starter built with Next.js';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://next-valhalla-starter.netlify.app'),
-  title: 'Next Valhalla Starter | AREA44',
-  description: 'An image gallery starter built with Next.js',
+  metadataBase: new URL(url),
+  title: title,
+  description: description,
+  icons: {
+    shortcut: '/favicon.svg',
+  },
   openGraph: {
-    title: 'Next Valhalla Starter | AREA44',
-    description: 'An image gallery starter built with Next.js',
-    url: 'https://next-valhalla-starter.netlify.app',
-    siteName: 'Next Valhalla Starter | AREA44',
+    title: title,
+    description: description,
+    url: url,
+    siteName: title,
     images: [
       {
         url: '/og-image.jpg',
@@ -20,39 +27,33 @@ export const metadata: Metadata = {
     locale: 'en-US',
     type: 'website',
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: ['/og-image.jpg'],
+  },
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: false,
-      noimageindex: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
       'max-snippet': -1,
     },
   },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Next Valhalla Starter | AREA44',
-    description: 'An image gallery starter built with Next.js',
-    creator: '@torn4dom4n',
-    images: ['/og-image.jpg'],
-  },
-  icons: {
-    shortcut: '/favicon.svg',
-  },
-}
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-black antialiased">{children}</body>
+    <html lang='en'>
+      <body className='bg-black antialiased'>{children}</body>
     </html>
-  )
+  );
 }
