95921a93ae63b0c73852b438477dbf13536bd91e
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-11 14:44:35 +0700

    easy config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,48 @@
-import './global.css'
-import type { Metadata } from 'next'
+import './global.css';
+import type { Metadata } from 'next';
+
+const url = 'https://next-valhalla-starter.netlify.app';
+const title = 'Next Valhalla Starter | AREA44';
+const description = 'An image gallery starter built with Next.js';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://next-valhalla-starter.netlify.app'),
-  title: 'Next Valhalla Starter | AREA44',
-  description: 'An image gallery starter built with Next.js',
+  metadataBase: new URL(url),
+  title: title,
+  description: description,
   openGraph: {
-    title: 'Next Valhalla Starter | AREA44',
-    description: 'An image gallery starter built with Next.js',
-    url: 'https://next-valhalla-starter.netlify.app',
-    siteName: 'Next Valhalla Starter | AREA44',
-    images: [
-      {
-        url: '/og-image.jpg',
-        width: 1920,
-        height: 1080,
-      },
-    ],
+    title: title,
+    description: description,
+    url: url,
+    siteName: title,
     locale: 'en-US',
     type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
   },
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: false,
-      noimageindex: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
       'max-snippet': -1,
     },
   },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Next Valhalla Starter | AREA44',
-    description: 'An image gallery starter built with Next.js',
-    creator: '@torn4dom4n',
-    images: ['/og-image.jpg'],
-  },
-  icons: {
-    shortcut: '/favicon.svg',
-  },
-}
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-black antialiased">{children}</body>
+    <html lang='en'>
+      <body className='bg-black antialiased'>{children}</body>
     </html>
-  )
+  );
 }
