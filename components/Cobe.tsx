aad97c0b31058c3e1e6b980758e771cacd0f6d44
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-21 02:11:12 +0700

    good

--- components/Cobe.tsx
+++ components/Cobe.tsx
@@ -1,19 +1,22 @@
 'use client'
+
 import createGlobe from 'cobe'
 import { useEffect, useRef } from 'react'
 
-export default function App() {
+export default function Cobe() {
   const canvasRef = useRef()
 
   useEffect(() => {
     let phi = 0
+    let width = 600
 
+    // Full API: https://cobe.vercel.app/docs/api
     const globe = createGlobe(canvasRef.current, {
       devicePixelRatio: 2,
-      width: 600 * 2,
-      height: 600 * 2,
+      width: width * 2,
+      height: width * 2,
       phi: 0,
-      theta: 0,
+      theta: -0.1,
       dark: 1,
       diffuse: 1.2,
       mapSamples: 16000,
@@ -21,7 +24,7 @@ export default function App() {
       baseColor: [0.3, 0.3, 0.3],
       markerColor: [0.1, 0.8, 1],
       glowColor: [1, 1, 1],
-      markers: [],
+      markers: [{ location: [21.18541, 106.07503], size: 0.04 }],
       onRender: (state) => {
         state.phi = phi
         phi += 0.01
