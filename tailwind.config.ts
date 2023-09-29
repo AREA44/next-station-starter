0567ad7ff6f7124e08c19efca25dd9c470369f8a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-30 00:08:23 +0700

    improve: use ts for tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
-/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
   content: [
@@ -63,12 +63,12 @@ export default {
       },
       keyframes: {
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
@@ -77,5 +77,5 @@ export default {
       },
     },
   },
-  plugins: ['tailwindcss-animate'],
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
