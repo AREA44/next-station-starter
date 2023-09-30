a0c9991f71e9ff5731a5652acbf7ebb91ede0c44
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-10-01 01:25:48 +0700

    feat: add routing and SEO file

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center text-center">
+      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        Something went wrong!
+      </h2>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  )
+}
